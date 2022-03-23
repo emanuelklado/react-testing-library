6ba33ba569bfe46b4cb6c3cc577d2df0972b934a
emanuelklado <emanuelklado@msn.com>
2022-03-23 18:00:46 -0300

    requisito 04 ok - stryker 100%

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import React from 'react';
+import { NotFound } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Teste se página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    screen.logTestingPlaygroundURL();
+    const notFound = screen.getByRole('heading', {
+      level: 2,
+      name: /page requested not found/i,
+    });
+    const emoji = screen.getByRole('img', {
+      name: /crying emoji/i,
+    });
+    const imgURL = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const img = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    expect(notFound).toBeInTheDocument();
+    expect(emoji).toBeInTheDocument();
+    expect(img).toBeInTheDocument();
+    expect(img.src).toEqual(imgURL);
+  });
+});
