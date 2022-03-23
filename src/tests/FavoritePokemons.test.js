d499b274e16915fa78a9cfe79abc514ea096536f
emanuelklado <emanuelklado@msn.com>
2022-03-23 17:36:19 -0300

    requisito 03 - com mais verificaçoes e stryker 100%

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -2,6 +2,7 @@ import { screen } from '@testing-library/react';
 import React from 'react';
 import { FavoritePokemons } from '../components';
 import renderWithRouter from '../renderWithRouter';
+import pokemons from '../data';
 
 describe('Teste o componente <FavoritePokemons.js />', () => {
   it('Teste se é exib a msg "No favorite pokemon found",se n tiver pok favoritos', () => {
@@ -9,5 +10,12 @@ describe('Teste o componente <FavoritePokemons.js />', () => {
     const notFoundFavorites = screen.getByText(/no favorite pokemon found/i);
     expect(notFoundFavorites).toBeInTheDocument();
     // screen.logTestingPlaygroundURL();
+  });
+  it('Teste se é exibido os cards dos pokemons favoritos', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ pokemons } />);
+    pokemons.forEach((elem) => {
+      const pokemonName = screen.getByText(elem.name);
+      expect(pokemonName).toBeInTheDocument();
+    });
   });
 });
