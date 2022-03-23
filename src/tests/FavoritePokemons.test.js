9c59d2bbedee5867dc243d244cd18edc69e201c5
emanuelklado <emanuelklado@msn.com>
2022-03-23 16:55:21 -0300

    mais uma tentativa evaluator - requisito 03 ok - stryker 100%

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -5,20 +5,9 @@ import renderWithRouter from '../renderWithRouter';
 
 describe('Teste o componente <FavoritePokemons.js />', () => {
   it('Teste se é exib a msg "No favorite pokemon found",se n tiver pok favoritos', () => {
-    const { history } = renderWithRouter(<FavoritePokemons />);
-    history.push('/Favorites');
-
+    renderWithRouter(<FavoritePokemons />);
     const notFoundFavorites = screen.getByText(/no favorite pokemon found/i);
     expect(notFoundFavorites).toBeInTheDocument();
     // screen.logTestingPlaygroundURL();
   });
-
-  //   it('Teste se é exib a msg "No favorite pokemon found",se n tiver pok favoritos', () => {
-  //     const { history } = renderWithRouter(<FavoritePokemons />);
-  //     history.push('/Favorites');
-
-//     const notFoundFavorites = screen.getByText(/no favorite pokemon found/i);
-//     expect(notFoundFavorites).toBeInTheDocument();
-//     // screen.logTestingPlaygroundURL();
-//   });
 });
